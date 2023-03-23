6bdd05632b158234124d6dc00cba285bef4efbf1
mert arın <mertkarin2@gmail.com>
2023-03-23 14:20:14 -0500

    fenerbahce

---
+++ src/first_git_package/classs2.java
@@ -0,0 +1,8 @@
+package first_git_package;
+
+public class classs2 {
+    public static void main(String[] args) {
+        System.out.println("honda accord full kasa");
+        System.out.println("honda cr-v full kasa");
+    }
+}
